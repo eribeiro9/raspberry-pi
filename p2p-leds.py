4ea6672868484f70e53af996a705659f5ec18850
Eric Ribeiro <eric.james.ribeiro@gmail.com>
2019-01-05 20:50:17 -0500

    Added script and main loop

--- p2p-leds.py
+++ p2p-leds.py
@@ -3,24 +3,46 @@ from TcpServerNode import Node
 import time
 
 # CONFIG
-MY_LED_PIN = 17
-MY_BUTTON_PIN = 19
-MY_IP = 'localhost'
-MY_PORT = 10000
-OTHER_LED_PIN = 27
-OTHER_IP = '1.2.3.4'
-OTHER_PORT = 10000
+LOCAL_LED_PIN = 17
+LOCAL_BUTTON_PIN = 19
+LOCAL_IP = 'localhost'
+LOCAL_PORT = 10000
+REMOTE_LED_PIN = 27
+REMOTE_IP = '1.2.3.4'
+REMOTE_PORT = 10000
 SLEEP_SEC = 0.2
 
 # VARIABLES
 node = None
-other_button_pressed = False
+remote_button_pressed = False
 
 # SETUP GPIO
-my_led = LED(MY_LED_PIN)
-my_button = Button(MY_BUTTON_PIN)
-other_led = LED(OTHER_LED_PIN)
+local_led = LED(LOCAL_LED_PIN)
+local_button = Button(LOCAL_BUTTON_PIN)
+remote_led = LED(REMOTE_LED_PIN)
 
 # TODO: NETWORK CALLBACK
+def on_network_callback:
+    # HANDLE EVENT
 
 # SETUP P2P NETWORK
+node = Node(LOCAL_IP, LOCAL_PORT, on_network_callback)
+node.start()
+node.connect_with_node(REMOTE_IP, REMOTE_PORT)
+
+# MAIN LOOP
+while (1):
+    # HANDLE LOCAL LED
+    if (local_button.is_pressed):
+        local_led.on()
+        # TODO: SEND NETWORK
+    else:
+        local_led.off()
+    
+    # HANDLE REMOTE LED
+    if (remote_button_pressed):
+        remote_led.on()
+    else:
+        remote_led.off()
+    
+    time.sleep(SLEEP_SEC)
