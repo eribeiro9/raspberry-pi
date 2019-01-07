1a1fe4800c9c7d2171cf7d513d9113c97bdd5435
Eric Ribeiro <eric.james.ribeiro@gmail.com>
2019-01-06 19:12:20 -0500

    Socket and threading

--- p2p-leds.py
+++ p2p-leds.py
@@ -1,61 +1,70 @@
 import RPi.GPIO as GPIO
-from TcpServerNode import Node
-import time
+import socket, _thread, time
 
 # CONFIG
 LOCAL_LED_PIN = 11 #GPIO17
 LOCAL_BUTTON_PIN = 35 #GPIO19
-LOCAL_IP = 'localhost'
 LOCAL_PORT = 10000
 REMOTE_LED_PIN = 13 #GPIO27
-REMOTE_IP = '192.168.1.158'#'73.128.178.46'
+REMOTE_IP = '192.168.1.158'#'192.168.1.158'#'73.128.178.46'
 REMOTE_PORT = 10000
-SLEEP_SEC = 0.2
 
 # VARIABLES
-node = None
+connection_from_remote = False
 connected_to_remote = False
-remote_button_pressed = False
+remote = None
 
 # SETUP GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(LOCAL_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup([LOCAL_LED_PIN, REMOTE_LED_PIN], GPIO.OUT, initial=GPIO.LOW)
 
-# NETWORK CALLBACK
-def on_network_callback(event, node, other, data):
-    global connected_to_remote
-    global remote_button_pressed
-    print("Event Node 1 (" + node.id + "): %s: %s" % (event, data))
-    if event == "CONNECTEDWITHNODE":
-        connected_to_remote = True
-    elif event == "NODEINBOUNDCLOSED" or event == "NODEOUTBOUNDCLOSED":
-        connected_to_remote = False
-    elif event == "NODEMESSAGE":
-        remote_button_pressed = data.value
-
-# SETUP P2P NETWORK
-def setup_node():
-    global node
-    node = Node(LOCAL_IP, LOCAL_PORT, on_network_callback)
-    node.start()
-    node.connect_with_node(REMOTE_IP, REMOTE_PORT)
-setup_node()
-
 def button_press(pin):
+    global connected_to_remote, client
     GPIO.output(LOCAL_LED_PIN, not GPIO.input(pin))
-    try:
-        node.send_to_nodes({ "value": not GPIO.input(pin) })
-    except:
-        setup_node()
-        pass
+    if connected_to_remote:
+        client.sendall(bytes(not GPIO.input(pin)))
 
 GPIO.add_event_detect(LOCAL_BUTTON_PIN, GPIO.BOTH, callback=button_press)
 
-# MAIN LOOP
-try:
-    while (1):
-        time.sleep(1)
+# START TCP SERVER
+def create_server():
+    global connection_from_remote, remote
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('', LOCAL_PORT))
+    server.listen(1)
+    remote, addr = server.accept()
+    print('Connected', addr)
+    connection_from_remote = True
+    
+_thread.start_new_thread(create_server, ())
 
-finally:
-    GPIO.cleanup()
+# START CONNECTION TO REMOTE
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+while not connected_to_remote:
+    try:
+        client.connect((REMOTE_IP, REMOTE_PORT))
+        connected_to_remote = True
+        
+    except:
+        pass
+
+# MAIN LOOP
+while 1:
+    try:
+        if connection_from_remote:
+            data = remote.recv(1024)
+
+            if not data: break
+
+            print('Client Says', data)
+
+    except socket.error:
+        print('Error Occured.')
+        break
+    
+    finally:
+        time.sleep(1e6)
+
+conn.close()
+GPIO.cleanup()
